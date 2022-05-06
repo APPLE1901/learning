72567707183c65287dc472d062ccd0ae34f84a6e
[ APPLE ] <89185695+APPLE1901@users.noreply.github.com>
2022-05-06 23:42:10 +0900

    Update jsjs.js

--- jsjs.js
+++ jsjs.js
@@ -4,7 +4,7 @@ function B1(){
 		alert(`안돼. 돌아가.`);
 	} else if(GetName >= 14 && GetName <= 30){
 		alert(`환영합니다!`);
-		location.href = 'www.youtube.com';
+		location.href = 'https://www.youtube.com/index';
 	} else if(GetName > 30){
 		alert(`구라ㄴ`)
 	} else if(GetName == 0){
