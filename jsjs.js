9500b96b02abcde7235b4db412271454b2439484
[ APPLE ] <89185695+APPLE1901@users.noreply.github.com>
2022-05-06 23:11:22 +0900

    Update jsjs.js

---
+++ jsjs.js
@@ -0,0 +1,15 @@
+function B1(){
+	let GetName = prompt("나이를 입력해 주십시오.");
+	if(GetName < 14 && GetName>0){
+		alert(`안돼. 돌아가.`);
+	} else if(GetName >= 14 && GetName <= 30){
+		alert(`환영합니다!`);
+		location.href = 'h.youtube.com/channel/UCKraQ8YoNZupNbgmW3gQkoA';
+	} else if(GetName > 30){
+		alert(`구라ㄴ`)
+	} else if(GetName == 0){
+		alert(`나이를 입력하라니깬.`)
+	} else {
+		alert(`나이를 다시 입력해 주십시오.`)
+	}
+}
